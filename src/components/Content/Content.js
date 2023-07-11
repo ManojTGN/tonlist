0cb78e9333ed06134e25fc9c7f04dcc639ae3e56
ManojTGN <manoj.thunderviz@gmail.com>
2023-07-11 14:34:24 +0530

    update button

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,5 +1,5 @@
 
-import { faHouse, faRectangleList } from '@fortawesome/free-solid-svg-icons';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
@@ -7,18 +7,7 @@ function Content(){
     return(
         <div className={`h-full row-span-16 grid grid-rows-3 grid-flow-col gap-4`}>
             <div className={`row-span-3 rounded w-full h-full bg-gradient-to-r from-zinc-800 to-green-300`}>
-                <div className={`h-12 rounded bg-gradient-to-r from-emerald-300 to-green-300`}>
-                    <div className={`h-full flex`}>
-                    <div className={`w-12 h-full flex items-center justify-center`}><FontAwesomeIcon className={`text-zinc-800 text-2xl`} icon={faHouse} /></div>
-                    <div className={`w-full h-full font-sans font-bold text-zinc-800 flex items-center`}>Home</div>
-                    </div>
-                </div>
-                <div className={`h-12 rounded bg-gradient-to-r from-emerald-300 to-green-300`}>
-                    <div className={`h-full flex`}>
-                    <div className={`w-10 h-full flex items-center justify-center`}><FontAwesomeIcon className={`text-zinc-800 text-2xl`} icon={faRectangleList} /></div>
-                    <div className={`w-full h-full font-sans font-bold text-zinc-800 flex items-center`}>Playlist</div>
-                    </div>
-                </div>
+                <button className={`flex w-full items-center justify-start rounded-md border border-transparent bg-gradient-to-r from-emerald-400 to-green-300 px-6 py-3 font-sans font-bold text-zinc-800 shadow-sm`}><FontAwesomeIcon className={`text-zinc-800 text-l`} icon={faHouse} /> Home </button>
             </div>
             <div className={`col-span-2 rounded w-full h-full bg-gradient-to-r from-green-300 to-zinc-800`}>
 
